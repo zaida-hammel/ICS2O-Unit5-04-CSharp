314824083a2593eee42dffed1b787e82b49909bd
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-05-05 17:23:06 +0000

    edited whitespace

--- main.cs
+++ main.cs
@@ -7,17 +7,15 @@ class Program
         int userAge;
         var dayWeek = Console.ReadLine();
 
-        //input 
         Console.WriteLine("Write your age:");
         userAge = int.Parse(Console.ReadLine());
-        Console.WriteLine("Write the day of the week it is (Capital first letter):");
+        Console.WriteLine("Write day of the week it is (Capital first letter):");
         dayWeek = Console.ReadLine();
-       
-        //process
+
         if ((dayWeek == "Tuesday" || dayWeek == "Thursday") && (userAge > 12 && userAge < 21))
             Console.WriteLine("You have a student discount! Pay student pricing.");
         else
             Console.WriteLine("You do not have a student discount! Pay regular pricing.");
-       
+
     }
 }
